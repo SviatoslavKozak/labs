9f6237d9a16fc76463ebf94686b7eb03a5ccfe23
svyatoslav <svyatoslav12381@gmail.com>
2020-05-05 02:14:32 +0300

    added new line

--- 6/lab_6.sh
+++ 6/lab_6.sh
@@ -11,6 +11,11 @@ cat ~/labs/6/6.log 2> /dev/null
 
 #Middle level
 ping -c 10 i.ua >> ~/labs/6/6.log
-ping -c 10 a.a >> ~/labs/6/6.log
-RANDOM_NUMBER="$RANDOM"
+ping -c 10 a.a 2>> ~/labs/6/6.log
+RANDOM_NUMBER="$RANDOM" #command $RANDOM_NUMBER don't work.
 echo $RANDOM_NUMBER >> ~/labs/6/6.log
+vimtutor -c ":q!" | quit vimtutor
+#id=$(ps -A | grep vim | awk "{print $1}") && kill $id
+perl -ne "3..3 and print" ~/labs/6/6.log
+ 
+
