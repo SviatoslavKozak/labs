d02327b029a361c7d43e1a7f041136b7e58f2922
svyatoslav <svyatoslav12381@gmail.com>
2020-05-05 19:12:33 +0300

    new lab

--- 6/lab_6.sh
+++ 6/lab_6.sh
@@ -14,8 +14,14 @@ ping -c 10 i.ua >> ~/labs/6/6.log
 ping -c 10 a.a 2>> ~/labs/6/6.log
 RANDOM_NUMBER="$RANDOM" #command $RANDOM_NUMBER don't work.
 echo $RANDOM_NUMBER >> ~/labs/6/6.log
-vimtutor -c ":q!" | quit vimtutor
-#id=$(ps -A | grep vim | awk "{print $1}") && kill $id
-perl -ne "3..3 and print" ~/labs/6/6.log
+#vimtutor | id=$(ps -A | grep vim | awk "{print $1}") && kill $id  #I tried to kill Vimtutor but failed. Method 2 "vimtutor -c q!" is also unsuccessful
+vim +3 ~/labs/6/6.log
+
+#Senior level
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+echo "please enter a site name!"
+read sitename
+
+ping -c 10 $sitename >> ~/labs/6/6.log | ping -c 10 $sitename 2>> ~/labs/6/6.error
  
 
