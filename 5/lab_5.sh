8da9d861bceb77b8c818ebb2289696be4c9a96fa
svyatoslav <svyatoslav12381@gmail.com>
2020-05-07 00:46:15 +0300

    lab 5

---
+++ 5/lab_5.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#Junior level
+man -f notify-send
+whatis notify-send
+man notify-send
+notify-send --help
+pinfo nano
+
+#Middle level
+man -k ssh | grep -i ssh | wc -l
+whatis nano && echo "displays a short description using the first line of the man command description" 
+which nano && echo "used to find the location of an executable which is in your path"
+whereis nano && echo "It doesn't only return the location of the executable but also of source file and man pages"
+ssh -V
+
+#Senior level
+apt-cache policy nano
+NANO_VERSION=$(nano -V)
+notify-send "Nano Version" "$NANO_VERSION"
+SSH_VERSION=$(ssh -V)
+notify-send "Ssh Version" "$SSH_VERSION" #need fix
+
